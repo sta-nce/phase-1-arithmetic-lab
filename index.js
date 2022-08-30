2042cf24c77b6b3dbbf2cd428ed1c6e60125afc6
Stanslous <stancemuindi21@gmail.com>
2022-08-30 20:42:59 +0300

    Completed assingment

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
-
 // Write your code here
+const num1 = 31;
+const num2 = 2;
+
+const multiply = num1*num2;
+
+const random = Math.ceil(Math.random());
+
+const num3 = 44;
+const num4 = 10;
+
+const mod = num3%num4;
+
+const max = Math.max(12,9,6,20);
+
+
+
